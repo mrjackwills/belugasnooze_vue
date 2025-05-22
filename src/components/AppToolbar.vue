3ff561e2dedddff635e57693accdf174e1d2adf2
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2025-05-22 09:28:07 +0100

    fix: titlebar text size

--- src/components/AppToolbar.vue
+++ src/components/AppToolbar.vue
@@ -1,7 +1,7 @@
 <template>
 	<v-toolbar :height='toolbarHeight' color='primary'>
 
-		<v-img src='@/assets/logo.svg' :eager='true' max-width='3rem' class='ml-2' />
+		<v-img src='@/assets/logo.svg' :eager='true' :max-width='maxWidth' class='ml-2' />
 
 		<v-toolbar-title :class='spacing' class='font-weight-bold'>
 			<span class='text-white text-no-wrap'>Beluga Snooze</span>
@@ -20,10 +20,11 @@ const { mobile } = useDisplay();
 
 const [loadingStore, userStore] = [loadingModule(), userModule()];
 
+const maxWidth = computed(() => mobile.value ? '2rem' : '3rem');
 const authenticated = computed(() => userStore.authenticated);
 const loading = computed(() => loadingStore.loading);
 const progressHeight = computed(() => mobile.value ? '4' : '8');
-const spacing = computed(() => mobile.value ? 'text-h6 py-2' : 'text-h5 my-1');
+const spacing = computed(() => mobile.value ? 'text-subtitle-1 py-2' : 'text-h5 py-1');
 const toolbarHeight = computed(() => mobile.value ? '56' : '70');
 
 </script>
